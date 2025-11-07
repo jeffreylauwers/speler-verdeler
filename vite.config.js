2d00db3a04e5183cb931b80529aa3f1c2a9d20b5
Jeffrey Lauwers <jeffreylauwers@gmail.com>
2025-11-07 13:24:10 +0100

    Clean up comments in vite.config.js
    Removed comments regarding repository name replacement.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// BELANGRIJK: Vervang 'REPOSITORY-NAME' met de naam van je GitHub repository
-// Bijvoorbeeld: als je repo 'soccer-team-balancer' heet, dan wordt dit:
-// base: '/soccer-team-balancer/'
-
 export default defineConfig({
   plugins: [react()],
   base: '/speler-verdeler/',
